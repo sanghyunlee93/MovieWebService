64720b0f839d12172e3163f78c0b032942451c88
sanghyunlee93 <okk0@naver.com>
2021-01-18 15:02:39 +0900

    Add Movie Detail

---
+++ src/components/Movies.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_yeat">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie_summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+Movie.protoType = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
